115aa3217f79a03122592f29f9f274cf308f3d81
iburilo <iburilo@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-10-28 11:54:35 +0000

    245872: Implement SVN specific ModelSynchronizeParticipant https://bugs.eclipse.org/bugs/show_bug.cgi?id=245872

--- branches/teamAPISupport/org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/synchronize/action/logicalmodel/RevertModelAction.java
+++ branches/teamAPISupport/org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/synchronize/action/logicalmodel/RevertModelAction.java
@@ -11,7 +11,11 @@
 
 package org.eclipse.team.svn.ui.synchronize.action.logicalmodel;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.team.core.synchronize.FastSyncInfoFilter;
+import org.eclipse.team.core.synchronize.SyncInfo;
 import org.eclipse.team.svn.core.operation.IActionOperation;
+import org.eclipse.team.svn.core.synchronize.AbstractSVNSyncInfo;
 import org.eclipse.team.svn.ui.synchronize.action.AbstractSynchronizeLogicalModelAction;
 import org.eclipse.team.ui.synchronize.ISynchronizePageConfiguration;
 
@@ -25,13 +29,25 @@ public class RevertModelAction extends AbstractSynchronizeLogicalModelAction {
 	public RevertModelAction(String text, ISynchronizePageConfiguration configuration) {
 		super(text, configuration);
 	}
-
+	
+	protected boolean needsToSaveDirtyEditors() {
+		return false;
+	}
+	
+	protected FastSyncInfoFilter getSyncInfoFilter() {
+		return new FastSyncInfoFilter() {
+			public boolean select(SyncInfo info) {
+				return org.eclipse.team.svn.ui.action.local.RevertAction.SF_REVERTABLE_OR_NEW.accept(((AbstractSVNSyncInfo)info).getLocalResource());
+			}
+		};
+	}
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.team.svn.ui.synchronize.action.AbstractSynchronizeLogicalModelAction#getOperation()
 	 */
 	protected IActionOperation getOperation() {
-		// TODO Auto-generated method stub
-		return null;
+		IResource []changedResources = this.syncInfoSelector.getSelectedResources();
+		return org.eclipse.team.svn.ui.action.local.RevertAction.getRevertOperation(this.getConfiguration().getSite().getShell(), changedResources, changedResources);
 	}
 
 }
