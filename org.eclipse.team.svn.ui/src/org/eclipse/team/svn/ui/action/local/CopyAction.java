0e4a964ce266713eed4185250ba20af66fe3dfdc
agurov <agurov@ee007c2a-0a25-0410-9ab9-bf268980928c>
2014-02-06 21:16:13 +0000

    backport into 1.1.x-3.0.x version

---
+++ org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/action/local/CopyAction.java
@@ -0,0 +1,188 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2008 Polarion Software.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sergiy Logvin - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.team.svn.ui.action.local;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.team.svn.core.IStateFilter;
+import org.eclipse.team.svn.core.operation.CompositeOperation;
+import org.eclipse.team.svn.core.operation.IActionOperation;
+import org.eclipse.team.svn.core.operation.local.AddToSVNOperation;
+import org.eclipse.team.svn.core.operation.local.RefreshResourcesOperation;
+import org.eclipse.team.svn.core.operation.local.refactor.CopyResourceOperation;
+import org.eclipse.team.svn.core.operation.local.refactor.CopyResourceWithHistoryOperation;
+import org.eclipse.team.svn.core.resource.ILocalResource;
+import org.eclipse.team.svn.core.utility.FileUtility;
+import org.eclipse.team.svn.ui.SVNUIMessages;
+import org.eclipse.team.svn.ui.action.AbstractWorkingCopyAction;
+import org.eclipse.team.svn.ui.dialog.DefaultDialog;
+import org.eclipse.team.svn.ui.panel.local.ContainerSelectionPanel;
+
+/**
+ * Copy local resource from one location to another with history if resource is versioned
+ * 
+ * @author Sergiy Logvin
+ */
+public class CopyAction extends AbstractWorkingCopyAction {
+	
+	public CopyAction() {
+		super();
+	}
+		
+	public void runImpl(IAction action) {
+		IResource []AllResources = this.getSelectedResources(CopyAction.SF_EXCLUDE_DELETED_AND_PROJECTS);
+		
+		HashMap<String, IResource> resourcesWithoutEqualsNames = new HashMap<String, IResource>();
+		HashSet<String> conflictedResources = this.excludeResourcesWithEqualNames(resourcesWithoutEqualsNames, AllResources);
+		
+		if (resourcesWithoutEqualsNames.isEmpty()) {
+			MessageDialog dialog = new MessageDialog(this.getShell(), SVNUIMessages.CopyAction_Conflict_Title, null, 
+					SVNUIMessages.CopyAction_Conflict_Message, 
+					MessageDialog.WARNING, 
+					new String[] {IDialogConstants.OK_LABEL}, 
+					0);
+			dialog.open();
+			return;
+		}
+		//make new filtered resources list without resources with equal names
+		final IResource []resources = resourcesWithoutEqualsNames.values().toArray(new IResource[resourcesWithoutEqualsNames.values().size()]);
+
+		ContainerSelectionPanel panel = new ContainerSelectionPanel(resources, conflictedResources);
+		DefaultDialog dialog = new DefaultDialog(this.getShell(), panel);
+		if (dialog.open() == 0) {
+			IPath path = panel.getSelectedPath();
+			boolean saveHistory = panel.isCopyWithHistorySelected();
+			
+			CompositeOperation op = new CompositeOperation("Operation_CopyResources", SVNUIMessages.class); //$NON-NLS-1$
+			
+			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+			IActionOperation addOp = null;
+			if (panel.isOverrideResourceName()) {
+				path = path.append(panel.getOverridenName());
+				if (resources.length == 1) {
+					IResource destination = resources[0].getType() == IResource.FILE ? (IResource)root.getFile(path) : root.getFolder(path);
+					String dirPath = FileUtility.getWorkingCopyPath(destination.getParent());
+					if (dirPath != null) {
+						new File(dirPath).mkdirs();
+					}
+					boolean []checkSave = new boolean[] {saveHistory};
+					IActionOperation copyOp = this.getCopyOperation(resources[0], checkSave, destination);
+					if (checkSave[0]) {
+						IResource []parents = FileUtility.getOperableParents(new IResource[] {destination}, IStateFilter.SF_UNVERSIONED, true);
+						if (parents.length > 0) {
+							addOp = new AddToSVNOperation(parents);
+						}
+					}
+					if (addOp != null) {
+						op.add(addOp);
+						op.add(copyOp, new IActionOperation[] {addOp});
+					}
+					else {
+						op.add(copyOp);
+					}
+				}
+				else {
+					IResource destination = root.getFolder(path);
+					String dirPath = FileUtility.getWorkingCopyPath(destination);
+					if (dirPath != null) {
+						new File(dirPath).mkdirs();
+					}
+					IResource []parents = FileUtility.addOperableParents(new IResource[] {destination}, IStateFilter.SF_UNVERSIONED, true);
+					if (parents.length > 0) {
+						addOp = new AddToSVNOperation(parents);
+						op.add(addOp);
+					}
+				}
+			}
+			
+			if (resources.length > 1 || !panel.isOverrideResourceName()) {
+				for (int i = 0; i < resources.length; i++) {
+					IPath tPath = path.append(resources[i].getName());
+					IResource target = resources[i].getType() == IResource.FILE ? (IResource)root.getFile(tPath) : root.getFolder(tPath);
+					IActionOperation copyOp = this.getCopyOperation(resources[i], new boolean[] {saveHistory}, target);
+					if (addOp != null) {
+						op.add(copyOp, new IActionOperation[] {addOp});
+					}
+					else {
+						op.add(copyOp);
+					}
+				}
+			}
+			
+			op.add(new RefreshResourcesOperation(new IResource[] {root.findMember(panel.getSelectedPath())}));
+			
+			this.runScheduled(op);
+		}
+	}
+	
+	protected IActionOperation getCopyOperation(IResource resource, boolean []saveHistory, IResource destination) {
+		if (saveHistory[0]) {
+			CopyResourceWithHistoryOperation copyOp = new CopyResourceWithHistoryOperation(resource, destination);
+			if (copyOp.isAllowed()) {
+				return copyOp;
+			} 
+			saveHistory[0] = false;
+		}
+		return new CopyResourceOperation(resource, destination);
+	}
+	
+	public boolean isEnabled() {
+		return this.checkForResourcesPresence(CopyAction.SF_EXCLUDE_DELETED_AND_PROJECTS); 
+	}
+	
+	protected boolean needsToSaveDirtyEditors() {
+		return true;
+	}
+	
+	protected static final IStateFilter SF_EXCLUDE_DELETED_AND_PROJECTS = new IStateFilter.AbstractStateFilter() {
+        protected boolean acceptImpl(ILocalResource local, IResource resource, String state, int mask) {
+        	if (!IStateFilter.SF_LINKED.accept(resource, state, mask) && !IStateFilter.SF_OBSTRUCTED.accept(resource, state, mask)) {
+        		return ((resource instanceof IFolder || resource instanceof IFile) && state != IStateFilter.ST_DELETED && state != IStateFilter.ST_MISSING);	
+        	}
+        	return false;
+        }
+		protected boolean allowsRecursionImpl(ILocalResource local, IResource resource, String state, int mask) {
+			return state != IStateFilter.ST_LINKED && state != IStateFilter.ST_OBSTRUCTED;
+		}
+    };
+
+    protected HashSet<String> excludeResourcesWithEqualNames(HashMap<String, IResource> map, IResource []resources) {
+    	HashSet<String> conflicts = new HashSet<String>();
+    	for (int i = 0; i < resources.length; i++) {
+			if (map.containsKey(resources[i].getName())) {
+				conflicts.add(resources[i].getName());
+			}
+			else {
+				map.put(resources[i].getName(), resources[i]);
+			}
+		}
+		//delete all conflicts from resources set 
+		for (Iterator<String> iter = conflicts.iterator(); iter.hasNext();) {
+			String element = iter.next();
+			map.remove(element);
+		}
+		return conflicts;
+    }
+
+}
---
+++ org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/action/local/UpdateAction.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2008 Polarion Software.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Alexander Gurov - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.team.svn.ui.action.local;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.team.svn.core.IStateFilter;
+import org.eclipse.team.svn.core.connector.ISVNConnector;
+import org.eclipse.team.svn.core.connector.SVNRevision;
+import org.eclipse.team.svn.core.operation.CompositeOperation;
+import org.eclipse.team.svn.core.operation.IActionOperation;
+import org.eclipse.team.svn.core.operation.local.RefreshResourcesOperation;
+import org.eclipse.team.svn.core.operation.local.RestoreProjectMetaOperation;
+import org.eclipse.team.svn.core.operation.local.SaveProjectMetaOperation;
+import org.eclipse.team.svn.core.operation.local.UpdateOperation;
+import org.eclipse.team.svn.core.resource.ILocalResource;
+import org.eclipse.team.svn.core.svnstorage.ResourcesParentsProvider;
+import org.eclipse.team.svn.ui.SVNTeamUIPlugin;
+import org.eclipse.team.svn.ui.SVNUIMessages;
+import org.eclipse.team.svn.ui.action.AbstractRecursiveTeamAction;
+import org.eclipse.team.svn.ui.dialog.DefaultDialog;
+import org.eclipse.team.svn.ui.operation.ClearUpdateStatusesOperation;
+import org.eclipse.team.svn.ui.operation.NotifyUnresolvedConflictOperation;
+import org.eclipse.team.svn.ui.panel.local.ResourceListPanel;
+import org.eclipse.team.svn.ui.preferences.SVNTeamPreferences;
+import org.eclipse.team.svn.ui.utility.UnacceptableOperationNotificator;
+
+/**
+ * Team services menu update action implementation
+ * 
+ * @author Alexander Gurov
+ */
+public class UpdateAction extends AbstractRecursiveTeamAction {
+
+	public static IStateFilter SF_MISSING_RESOURCES = new IStateFilter.AbstractStateFilter() {
+		protected boolean acceptImpl(ILocalResource local, IResource resource, String state, int mask) {
+			//we shouldn't take into account resources with tree conflicts here 
+			return IStateFilter.SF_MISSING.accept(resource, state, mask) && !IStateFilter.SF_TREE_CONFLICTING.accept(resource, state, mask);
+		}
+		protected boolean allowsRecursionImpl(ILocalResource local, IResource resource, String state, int mask) {
+			return IStateFilter.SF_ONREPOSITORY.accept(resource, state, mask) || IStateFilter.SF_UNVERSIONED_EXTERNAL.accept(resource, state, mask);
+		}			
+	};
+	
+	public UpdateAction() {
+		super();
+	}
+
+	public void runImpl(IAction action) {
+		IResource []resources = UnacceptableOperationNotificator.shrinkResourcesWithNotOnRespositoryParents(this.getShell(), this.getSelectedResources(IStateFilter.SF_ONREPOSITORY));
+		if (resources == null || resources.length == 0) {
+			return;
+		}
+		
+		if (this.checkForResourcesPresenceRecursive(IStateFilter.SF_REVERTABLE)) {
+			IResource []missing = this.getSelectedResourcesRecursive(UpdateAction.SF_MISSING_RESOURCES);
+			if (missing.length > 0 && !UpdateAction.updateMissing(this.getShell(), missing)) {
+				return;
+			}
+		}
+		
+		this.runScheduled(UpdateAction.getUpdateOperation(resources, SVNRevision.HEAD));
+	}
+	
+	public boolean isEnabled() {
+		return this.checkForResourcesPresence(IStateFilter.SF_ONREPOSITORY);
+	}
+	
+	public static boolean updateMissing(Shell shell, IResource []missing) {
+		ResourceListPanel panel = new ResourceListPanel(missing, SVNUIMessages.UpdateAction_List_Title, SVNUIMessages.UpdateAction_List_Description, SVNUIMessages.UpdateAction_List_Message, new String[] {IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL});
+		panel.setShowLocalNames(true);
+		return new DefaultDialog(shell, panel).open() == 0;
+	}
+	
+	public static CompositeOperation getUpdateOperation(IResource []updateSet, SVNRevision selectedRevision) {
+		return UpdateAction.getUpdateOperation(updateSet, selectedRevision, ISVNConnector.Depth.INFINITY, false, null);
+	}
+	
+	public static CompositeOperation getUpdateOperation(IResource []updateSet, SVNRevision selectedRevision, int depth, boolean isStickyDepth, String updateDepthPath) {
+		boolean ignoreExternals = SVNTeamPreferences.getBehaviourBoolean(SVNTeamUIPlugin.instance().getPreferenceStore(), SVNTeamPreferences.BEHAVIOUR_IGNORE_EXTERNALS_NAME);
+		UpdateOperation mainOp = new UpdateOperation(updateSet, selectedRevision, ignoreExternals);
+		mainOp.setDepthOptions(depth, isStickyDepth, updateDepthPath);
+		
+		CompositeOperation op = new CompositeOperation(mainOp.getId(), mainOp.getMessagesClass());
+		
+		SaveProjectMetaOperation saveOp = new SaveProjectMetaOperation(updateSet);
+		op.add(saveOp);
+		op.add(mainOp);
+		op.add(new RestoreProjectMetaOperation(saveOp));
+		op.add(new ClearUpdateStatusesOperation(mainOp), new IActionOperation[]{mainOp});
+		op.add(new RefreshResourcesOperation(new ResourcesParentsProvider(mainOp)/*, IResource.DEPTH_INFINITE, RefreshResourcesOperation.REFRESH_ALL*/));
+		op.add(new NotifyUnresolvedConflictOperation(mainOp));
+		
+		return op;
+	}
+	
+	protected boolean needsToSaveDirtyEditors() {
+		return true;
+	}
+	
+}
